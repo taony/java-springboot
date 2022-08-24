5badb5510764b8360e2bd1bfdf310e5f89d716fc
taony <taonyzhang@gmail.com>
2022-08-24 12:36:15 +0800

    feat：创建spring-boot-redis-stream工程

---
+++ springboot/springboot-redis-stream/src/main/java/com/taony/springboot/redis/RedisStreamApp.java
@@ -0,0 +1,16 @@
+package com.taony.springboot.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class RedisStreamApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisStreamApp.class, args);
+    }
+
+}
