033361c98605c80a9c8de6003dc3049315f345de
taony <taonyzhang@gmail.com>
2022-08-31 16:29:02 +0800

    feat：修改redis-stream

--- springboot/springboot-redis-stream/src/main/java/com/taony/springboot/redis/utils/RedisStreamUtil.java
+++ springboot/springboot-redis-stream/src/main/java/com/taony/springboot/redis/utils/RedisStreamUtil.java
@@ -10,69 +10,98 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.util.*;
 
+
 /**
- * @author Huhailong
- * @Description 添加stream消息
- * @Date 2021/3/10.
+ * Redis S
+ * @author taony
  */
 @Slf4j
 @Component
 public class RedisStreamUtil {
-    private final RedisTemplate<String,String> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Autowired
-    public RedisStreamUtil(RedisTemplate<String,String> redisTemplate){
+    public RedisStreamUtil(RedisTemplate<String, String> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
+
     /**
-     * XADD 添加stream消息
-     * @param key stream对应的key
-     * @param message 要村粗的消息数据
+     * 添加stream消息
+     *
+     * @param key
+     * @param message
+     * @return
      */
-    public RecordId addStream(String key, Map<String,Object> message){
-        RecordId add = redisTemplate.opsForStream().add(key, message);
-//        log.info("添加成功："+add);
-        return add;
+    public RecordId addStream(String key, Map<String, Object> message) {
+        return redisTemplate.opsForStream().add(key, message);
     }
 
-    public void addGroup(String key, String groupName){
-        redisTemplate.opsForStream().createGroup(key,groupName);
+    /**
+     * 添加分组
+     *
+     * @param key
+     * @param groupName
+     */
+    public void addGroup(String key, String groupName) {
+        redisTemplate.opsForStream().createGroup(key, groupName);
     }
 
-    public void delField(String key, String fieldId){
-        redisTemplate.opsForStream().delete(key,fieldId);
+    /**
+     * 删除消息
+     *
+     * @param key
+     * @param fieldId
+     */
+    public void delField(String key, String fieldId) {
+        redisTemplate.opsForStream().delete(key, fieldId);
     }
 
     /**
      * 读取所有stream消息
-     * @param key
-     * @return
      */
-    public List<MapRecord<String,Object,Object>> getAllStream(String key){
+    public List<MapRecord<String, Object, Object>> getAllStream(String key) {
         List<MapRecord<String, Object, Object>> range = redisTemplate.opsForStream().range(key, Range.open("-", "+"));
-        if(range == null){
+        if (range == null) {
             return null;
         }
-        for(MapRecord<String,Object,Object> mapRecord : range){
-            redisTemplate.opsForStream().delete(key,mapRecord.getId());
+        for (MapRecord<String, Object, Object> mapRecord : range) {
+            redisTemplate.opsForStream().delete(key, mapRecord.getId());
         }
         return range;
     }
 
-    public void getStream(String key){
-        List<MapRecord<String, Object, Object>> read = redisTemplate.opsForStream().read(StreamReadOptions.empty().block(Duration.ofMillis(1000*30)).count(2), StreamOffset.latest(key));
+    /**
+     * 获取消息
+     *
+     * @param key
+     */
+    public void getStream(String key) {
+        List<MapRecord<String, Object, Object>> read = redisTemplate.opsForStream()
+                .read(StreamReadOptions.empty().block(Duration.ofMillis(1000 * 30)).count(2), StreamOffset.latest(key));
         System.out.println(read);
     }
 
-    public void getStreamByGroup(String key, String groupName,String consumerName){
-        List<MapRecord<String, Object, Object>> read = redisTemplate.opsForStream().read(Consumer.from(groupName, consumerName), StreamReadOptions.empty(), StreamOffset.create(key,ReadOffset.lastConsumed()));
-        log.info("group read :{}",read);
+    /**
+     * 根据分组获取消息
+     *
+     * @param key
+     * @param groupName
+     * @param consumerName
+     */
+    public void getStreamByGroup(String key, String groupName, String consumerName) {
+        List<MapRecord<String, Object, Object>> read = redisTemplate.opsForStream().read(Consumer.from(groupName, consumerName), StreamReadOptions.empty(), StreamOffset.create(key, ReadOffset.lastConsumed()));
+        log.info("group read :{}", read);
     }
 
-    public boolean hasKey(String key){
-        Boolean aBoolean = redisTemplate.hasKey(key);
-        return aBoolean==null?false:aBoolean;
+    /**
+     * 验证是否存在Key
+     *
+     * @param key
+     * @return
+     */
+    public boolean hasKey(String key) {
+        return redisTemplate.hasKey(key) != null;
     }
 
 }
